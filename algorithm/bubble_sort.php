5a0b89b317aa1380684cc2f8eb5d713fa9060eb4
altwei <j@altwei.com>
2019-09-16 23:06:47 +0800

    bubble sort
    Signed-off-by: altwei <j@altwei.com>

---
+++ algorithm/bubble_sort.php
@@ -0,0 +1,44 @@
+<?php
+$data = [5, 2, 1, 8, 9, 3, 4, 7, 6,];
+/**
+ * 冒泡排序 - 升序
+ * @param array $data
+ * @return array
+ */
+function bubble_sort_asc($data = [])
+{
+
+    for ($i = 0; $i < count($data); $i++) {
+        for ($j = 0; $j < count($data) - $i - 1; $j++) {
+            if ($data[$j] > $data[$j + 1]) {
+                list($small, $big) = [$data[$j + 1], $data[$j]];
+                $data[$j] = $small;
+                $data[$j + 1] = $big;
+            }
+        }
+    }
+    return $data;
+}
+
+/**
+ * 冒泡排序 - 降序
+ * @param array $data
+ * @return array
+ */
+function bubble_sort_desc($data = [])
+{
+
+    for ($i = 0; $i < count($data); $i++) {
+        for ($j = 0; $j < count($data) - $i - 1; $j++) {
+            if ($data[$j] < $data[$j + 1]) {
+                list($small, $big) = [$data[$j], $data[$j + 1]];
+                $data[$j] = $big;
+                $data[$j + 1] = $small;
+            }
+        }
+    }
+    return $data;
+}
+
+echo implode(', ', bubble_sort_asc($data)), PHP_EOL;
+echo implode(', ', bubble_sort_desc($data)), PHP_EOL;
